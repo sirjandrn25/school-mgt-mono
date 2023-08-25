000f5b3b28ef62e61ac3f55f235502eb4ffa3f98
sirjandrn25 <56628702+sirjandrn25@users.noreply.github.com>
2023-08-25 07:14:26 +0545

    add authentication

--- apps/web/src/components/header.component.tsx
+++ apps/web/src/components/header.component.tsx
@@ -1,13 +1,30 @@
+import useLogout from "@hooks/useLogout.hook";
 import { UserCircle } from "lucide-react";
-import React from "react";
-import { InputField } from "ui";
+import { InputField, dropDownMenuItem } from "ui";
+import DropdownMenu from "ui/components/dropdown-menu/dropdown-menu.component";
 
 const Header = () => {
+    const { handleLogout } = useLogout();
+    const items: dropDownMenuItem[] = [
+        {
+            name: "Settings",
+            key: "settings",
+        },
+        {
+            name: "Logout",
+            key: "logout",
+            action: handleLogout,
+        },
+    ];
     return (
         <div className="h-[60px] bg-base-100 px-4 flex justify-between items-center gap-4 border-b">
             <InputField type="text" placeholder="Search" />
             <div className="flex items-center gap-2 pr-4">
-                <UserCircle className="cursor-pointer" />
+                <DropdownMenu
+                    align="end"
+                    trigger={<UserCircle />}
+                    items={items}
+                />
             </div>
         </div>
     );
